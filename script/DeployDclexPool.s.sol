1b3676ce80a22743b82853c134a647c622afbd2c
Wacławski <lukasz.waclawski@10clouds.com>
2025-03-11 18:44:12 +0100

    Fix HelperConfig import to be relative

--- script/DeployDclexPool.s.sol
+++ script/DeployDclexPool.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {DclexPool} from "../src/DclexPool.sol";
 import {IStock} from "dclex-blockchain/contracts/interfaces/IStock.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -27,5 +27,12 @@ contract DeployDclexPool is Script {
         );
         vm.stopBroadcast();
         return dclexPool;
+    }
+
+    function run() external {
+        address stock = (block.chainid == 11155111)
+            ? 0x538d1094A35201D69e1Ac8c2dD42000C1CC0612E
+            : 0x7fc1375aA5d360Ca90cc443B5c3d3919aA8B9208;
+        this.run(IStock(address(stock)), new HelperConfig());
     }
 }
